031487db1f889f7d3a3e784c27b7e18f1725dcf1
Richard Ramos <rramos@Richards-MacBook-Pro.local>
2018-07-25 21:54:43 -0700

    Commit New

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class FavoriteComponent implements OnInit {
   @Input('isfavorite') isFavorite: boolean;
